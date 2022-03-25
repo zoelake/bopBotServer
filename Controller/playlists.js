ce59163dc4a0e295b6eeb822fd3acf9fec2d09ae
zoelake <zoe_lake@icloud.com>
2022-03-25 13:48:16 -0700

    users can now update playlist imgs

--- Controller/playlists.js
+++ Controller/playlists.js
@@ -19,7 +19,7 @@ const createPlaylist = (req, res) => {
 
 //load current users playlists
 const getPlaylists = (req, res) => {
-    console.log(`getting ${req.body.user}'s playlists`)
+    // console.log(`getting ${req.body.user}'s playlists`)
     User.findOne({ email: req.body.user }, (err, user) => {
         if (err) return res.status(500).send('server error')
         if (user) {
@@ -43,16 +43,31 @@ const getAPlaylist = (req, res) => {
 const updatePlaylist = (req, res) => {
     console.log('updating:')
     console.log(req.body)
-    User.updateOne({ email: req.body.user, 'playlists.name': req.body.playlist_name },
-        {
-            '$set': {
-                'playlists.$.name': req.body.playlist_newName,
-                'playlists.$.img': req.body.playlist_img,
-            }
-        }, (err, user) => {
-            if (err || !user) return res.status(500).send('something went wrong')
-            res.status(200).send(`${req.body.playlist_name} was updated to ${req.body.playlist_newName}`);
-        })
+    if (req.body.playlist_newName !== null) {
+
+        User.updateOne({ email: req.body.user, 'playlists.name': req.body.playlist_name },
+
+            {
+                '$set': {
+                    'playlists.$.name': req.body.playlist_newName,
+                    'playlists.$.img': req.body.playlist_newImg,
+                }
+            }, (err, user) => {
+                if (err || !user) return res.status(500).send('something went wrong')
+                res.status(200).send(`${req.body.playlist_name} was updated to ${req.body.playlist_newName}`);
+            })
+    } else {
+        User.updateOne({ email: req.body.user, 'playlists.name': req.body.playlist_name },
+
+            {
+                '$set': {
+                    'playlists.$.img': req.body.playlist_newImg,
+                }
+            }, (err, user) => {
+                if (err || !user) return res.status(500).send('something went wrong')
+                res.status(200).send(`${req.body.playlist_name} was updated to ${req.body.playlist_newName}`);
+            })
+    }
 }
 
 // delete selected playlist 
