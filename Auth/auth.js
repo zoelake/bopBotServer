cb91a5bcbcd9e7cba25cae252954915c46dc8adf
zoelake <zoe_lake@icloud.com>
2022-05-23 17:48:47 -0700

    security update

--- Auth/auth.js
+++ Auth/auth.js
@@ -1,9 +1,9 @@
 const jwt = require('jsonwebtoken')
 //move my secrete to .env file
 
-const authorizeUser = (req,res,next) => {
-    jwt.verify(req.query.token, "thisismysecret", (err,data)=>{
-        if(err)return res.send('not authorized')
+const authorizeUser = (req, res, next) => {
+    jwt.verify(req.query.token, process.env.AUTH_KEY, (err, data) => {
+        if (err) return res.send('not authorized')
         req.user = data
         next()
     })
