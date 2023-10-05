4d5cad4949217a74d59e0b87b78e17ce45c614e7
zoelake <zoe_lake@icloud.com>
2023-10-04 19:34:08 -0700

    updates

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ const express = require('express')
 const app = express();
 const cors = require('cors')
 const mongoose = require('mongoose');
-
+require('dotenv').config()
 const playlistRouter = require('./Routes/playlist.js');
 const userRouter = require('./Routes/user.js');
 const trackRouter = require('./Routes/track.js');
